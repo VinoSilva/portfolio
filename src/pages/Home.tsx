9c9e8f70ac9e1691dfa3e6b4d9886df6ffbea8c0
Vino <VinoshenSilva@gmail.com>
2025-10-05 19:01:24 +0800

    Remove certificates

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 // Import components
-import CertificatesSection from "@components/Home/CertificatesSection";
+// import CertificatesSection from "@components/Home/CertificatesSection";
 import ExperiencesSection from "@components/Home/ExperiencesSection";
 import Hero from "@components/Home/Hero";
 import ProjectsSection from "@components/Home/ProjectsSection";
@@ -16,8 +16,8 @@ const Home = () => {
       <ExperiencesSection />
       <div className="mt-40" />
       <SkillsSection />
-      <div className="mt-40" />
-      <CertificatesSection />
+      {/* <div className="mt-40" />
+      <CertificatesSection /> */}
       <div className="mt-40" />
       <StorySection />
     </div>
